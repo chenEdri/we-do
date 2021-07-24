5959a8dfa0620f909c89620474bf57c4fa65c7d8
chen edri <chenedri22@gmail.com>
2021-07-24 17:07:58 +0300

    style

--- frontend/src/cmps/Footer.jsx
+++ frontend/src/cmps/Footer.jsx
@@ -1,36 +1,32 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import {Facebook, GitHub, LinkedIn } from '@material-ui/icons'
+
+function openSource(source) {
+    window.open(source);
+}
+
 
 export function Footer() {
     return (
-        <div className="brt main-container">
-            <div className=" flex sa">
+        <div className="footer brt">
+            <div className="footer-container">
                 <div className="foot-col">
-                <div className="bold"><p>ABOUT</p></div>
-                    <p>How Health-E works</p>
-                    <p>Newsroom</p>
-                    <p>Health-E Plus</p>
-                    <p>Healthy for Work</p>
-                    <p>Olympics</p>
-                    <p>Careers</p>
+                <div className="bold"><div>TECHNOLOGY</div></div>
+                    <p>React Hooks</p>
+                    <p>Node.js</p>
+                    <p>Mongo DB</p>  
                 </div>
                 <div className="foot-col">
-                    <div className="bold"><p>COMMUNITY</p></div>
-                    <p>Diversity Belonging</p>
-                    <p>Against Discrimination</p>
-                    <p>Accessibility</p>
-                    <p>Frontline Stays</p>
-                    <p>Invite friends</p>
-                    <p>Gift cards</p>
+                    <div className="bold"><div>CONTACT</div></div>
+                    <div className="mb10" ><a className="foot-hover mb10" href="mailto:chenedri22@gmail.com">chenedri22@gmail.com</a></div>        
                 </div>
                 <div className="foot-col">
-                    <div className="bold"><p>SUPPORT</p></div>
-                    {/* <Link to ="/about">About us</Link> */}
-                    <p>Updates for COVID-19</p>
-                    <p>Help Center</p>
-                    <p>Cancellation options</p>
-                    <p>Neighborhood Support</p>
-                    <p>Trust and Safety</p>
+                    <div className="bold"><div>Media</div></div>
+                    <div className="cp" onClick={() => openSource('https://www.linkedin.com/in/chen-edri-46290776')}><LinkedIn /></div>
+                        <div className="cp" onClick={() => openSource('https://github.com/chenEdri ')}><GitHub /></div>
+                        <div className="cp" onClick={() => openSource('https://www.facebook.com/chen.edri.3')}><Facebook /></div>
+                        <div className="cp" onClick={() => { 'https://api.whatsapp.com/send?phone=972537616607' }}><WhatsAppIcon /></div>
                 </div>
             </div>
         </div>
